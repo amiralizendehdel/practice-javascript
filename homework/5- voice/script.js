81da37d567cbcf1ce2e4738ed15914ff803b3d12
amiralizendehdel <a.zendehdel1234@gmail.com>
2021-08-28 16:39:06 +0430

    added a homework

---
+++ homework/5- voice/script.js
@@ -0,0 +1,107 @@
+// //HomWork 1
+// var users = ['hamid','ali','reza','mohsen'];
+
+// alert(users[1]);
+
+
+
+// //HomWork 2
+// var number = [12,32,44,100];
+
+// alert(number[0]);
+
+
+
+
+// //HomWork 3
+// var amir = {
+//     name: 'amir', 
+//     lastname: 'amiri',
+//     age: 30
+// };
+
+
+
+
+// //HomWork 4
+// var a = [134,54,23,15];
+
+// var b = ['hamid',33,'hello',45,87];
+
+
+
+
+// //HomWork 5
+// var c = [
+//     {name: 'ali' , age: 10},
+//     {name: 'hamid' , age: 25},
+//     {name: 'reza' , age: 30}
+// ];
+
+
+
+
+// //HomWork 6
+// var d = {
+//     function() { },
+
+//     message : 'javascript'
+
+//     ['mohsen','hamid','reza']
+// };
+
+
+
+
+// //HomWork 7
+// var g = {
+//     [22,'hi',{name: 'mohamad'}]
+// };
+
+
+
+// //HomWork 8
+// var v = {
+//     Name: "ahmad",
+//     Number: 12312,
+//     fun: function () {
+//       alert("this is a function");
+//     },
+//     Array: ["1400", "yes", "hi"],
+//     admin: true,
+//   };
+
+
+
+
+// //HomWork 9
+// function fn1() {
+//     document.write('this is a Function')
+// }
+
+// var obj = {
+//     name: 'hamid',
+//     age: '24'
+// }
+
+// var arr2 = ['html','css'];
+
+// var arr1 = ['javascript',20,obj,arr2,fn1];
+
+// alert(arr1[3][2]);
+// alert(arr1[2].name);
+// arr1[5]();
+
+
+
+
+// //HomWork 10
+// var z = [
+//     {},
+//     {
+//         function (){},
+//         function add(x,y){
+//             return x + y;
+//         }
+//     }
+// ];
